179410ec9b023c16872ce72b48e5c77d9cb29aed
Your Name <you@example.com>
2017-07-31 22:20:40 +0530

    this

---
+++ app/src/main/java/com/ceemart/ceemart/Modules/Signup/Beacon/Beacon.java
@@ -0,0 +1,15 @@
+package com.ceemart.ceemart.Modules.Signup.Beacon;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.ceemart.ceemart.R;
+
+public class Beacon extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_beacon);
+    }
+}
